257f11657313ea04a3fa4cbfd4c1104b2ba8212c
Nataliya Novak <novak.natali@gmail.com>
2022-10-21 00:51:07 +0300

    summary

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { App } from 'components/App';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename='/goit-react-hw-06-phonebook'>
+      <Provider store={store}> 
+        <PersistGate loading={ null} persistor={persistor}>
+           <App />
+        </PersistGate>
+        
+      </Provider>
+     
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+
+ export const contactsSlice = createSlice({
+    name: "contacts",
+     initialState: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        
+     ],
+     reducers:
+     {
+         addContacts(state, action) {
+             
+             console.log(state);
+             state.push(action.payload)
+          
+         },
+         deleteContacts(state, action) {
+             console.log(action)
+             console.log(state)
+             const index = state.findIndex(item => item.id === action.payload)
+             state.splice(index, 1)
+            
+         },
+     }
+ })
+
+ const persistConfig = {
+  key: 'contacts',
+  storage,
+}
+export const persistedReducer = persistReducer(persistConfig, contactsSlice.reducer);
+
+export const { addContacts, deleteContacts } = contactsSlice.actions;
